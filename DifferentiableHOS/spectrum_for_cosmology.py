96c02e895f8c417572ba5742f1673e61143122de
LANZIERI Denise <deniselanzieri@gmail.com>
2021-02-18 19:06:08 +0100

    cosmological dependence modified

--- DifferentiableHOS/spectrum_for_cosmology.py
+++ DifferentiableHOS/spectrum_for_cosmology.py
@@ -8,138 +8,52 @@ Created on Mon Feb 15 13:38:27 2021
 import numpy as np
 import tensorflow as tf
 import flowpm
-from flowpm.tfbackground import cosmo, rad_comoving_distance
-from flowpm.tfpm import kick, drift, force
-import flowpm.constants as constants
+from flowpm.tfbackground import cosmo,afactor,z2a,chifactor
+from flowpm.raytracing import  lightcone, Born
+from flowpm.angular_power_tf import measure_power_spectrum_tf
+from flowpm.tfpower import linear_matter_power
 
 
-def power_spectrum_for_cosmology(state, stages, nc,
-              plane_resolution, # in arcmin
-              plane_size, # in pixels
-              field,
-              Boxsize,
-              ds,
+
+#%%
+nc=[64,64,640]   # size of the cube, number of cells
+plane_size=64                    # number of pixel for x and  y 
+Boxsize=[200,200,2000]          # Physical size of the cube
+r = np.linspace(0,2000,10, endpoint=True)
+a = afactor(r)  
+a_s=z2a(1.00)
+ds=chifactor(a_s)
+field=5.
+a0=0.1
+af=1.0
+n_steps=10
+
+#%%
+
+
+@tf.function
+def power_spectrum_for_cosmology(
               Omega0_m,
-              sigma8,                 
-              cosmology=cosmo, pm_nc_factor=1, 
-              name="NBody"):
-    
-    cosmo['Omega0_m']=tf.cast(Omega0_m,dtype=tf.float32)
-    cosmo['sigma8']=tf.cast(sigma8,dtype=tf.float32)
-    #prefactor from Poisson equation
-    cons=tf.cast(3/2*Omega0_m*(cosmo['H0']/constants.c)**2,dtype=tf.float32)
-    #mean 3D particle density
-    nbar=np.prod(nc)/np.prod(Boxsize)
-    #2D mesh area in rad^2 per pixel
-    A=((field*np.pi/180/plane_size)**2) 
-    #resolution=field*60/plane_size
-    #pixel_size =  =pi * resolution / 180. / 60. #rad/pixel
-    pixel_size_tf=field/plane_size / 180 *np.pi 
-    with tf.name_scope(name):
-        state = tf.convert_to_tensor(state, name="state")
-
-        shape = state.get_shape()
-        batch_size = shape[1]
-
-        # Unrolling leapfrog integration to make tf Autograph happy
-        if len(stages) == 0:
-          return state
-
-        ai = stages[0]
-
-        # first force calculation for jump starting
-        state = force(state, nc, pm_nc_factor=pm_nc_factor, cosmology=cosmology)
-
-        # Compute the width of the lens planes based on number of time steps
-        w = nc[2]//(len(stages)-1)
-        nx = nc[0]
-        nz = nc[2]
-        lps = []
-        lps_a = []
-
-        x, p, f = ai, ai, ai
-        # Loop through the stages
-        for i in range(len(stages) - 1):
-            a0 = stages[i]
-            a1 = stages[i + 1]
-            ah = (a0 * a1) ** 0.5
-
-            # Kick step
-            state = kick(state, p, f, ah, cosmology=cosmology)
-            p = ah
-
-            # Drift step
-            state = drift(state, x, p, a1, cosmology=cosmology)
-            x = a1
-
-            # Access the positions of the particles
-            pos = state[0]
-            d = pos[:,:,2]      
-
-            # This is the transverse comoving distance inside the box
-            xy = pos[:,:,:2] - nx/2
-
-            # Compute density plane in sky coordinates around the center of the lightcone
-            # TODO: Confirm conversion from comoving distances to angular size! I thought
-            # we should be using the angular diameter distance, but as far as I can see
-            # everyone uses the transverse comoving distance, and I don't understand exactly why
-            lens_plane = tf.zeros([batch_size, plane_size, plane_size])
-
-            # Convert coordinates to angular coords, and then into plane coords
-            xy = (xy / tf.expand_dims(d,-1))/np.pi*180*60/plane_resolution
-            xy = xy + plane_size/2
-
-            # Selecting only the particles contributing to the lens plane
-            mask = tf.where((d>(nz - (i+1)*w)) & (d <= (nz - i*w)),1.,0.)
-            # And falling inside the plane, NOTE: This is only necessary on CPU, on GPU
-            # cic paint 2d can be made to work with non periodic conditions.
-            mask = mask * tf.where((xy[...,0]>0) & (xy[...,0]<plane_size),1.,0.)
-            mask = mask * tf.where((xy[...,1]>0) & (xy[...,1]<plane_size),1.,0.)
-            # Compute lens planes by projecting particles
-            lens_plane = flowpm.utils.cic_paint_2d(lens_plane, xy + plane_size/2 ,mask)
-            lps.append(lens_plane)
-            lps_a.append(ah)
-
-            # Here we could trim the state vector for particles originally beyond the current lens plane
-            # This way the simulation becomes smaller as it runs and we save resources
-            state = tf.reshape(state, [3,batch_size, nc[0], nc[1],-1, 3])
-            state = state[:,:,:,:,:(nz - i*w - w // 2),:] # We keep w/2 to be safe, so we allow particle to travel
-                                                         # A max distance of width/2
-            # redefine shape of state
-            nc = state.get_shape()[2:5]
-            state = tf.reshape(state, [3,batch_size,-1,3])
-            # So this seems to work, but we should be a tiny bit careful because we break periodicity in the z
-            # direction at z=0.... probably not a big deal but still gotta check what that does.
-
-
-            # Force
-            state = force(state, nc, pm_nc_factor=pm_nc_factor, cosmology=cosmology)
-            f = a1
-
-            # Kick again
-            state = kick(state, p, f, a1, cosmology=cosmology)
-            p = a1
-    d=rad_comoving_distance(cosmology,lps_a)
-    columndens =(A*nbar)*(d**2)#particles/Volume*angular pixel area* distance^2 -> 1/L units
-    w  = ((ds-d)*(d/ds))/(columndens)
-    w=w/lps_a  
-    #w=tf.cast(w,dtype=tf.float64)
-    k_map=0
-    for i in range(len(lps_a)):
-        k_map += cons*lps[i][0]*  w[i]
+              sigma8):
+    cosmology=cosmo
+    cosmology['Omega0_m']=tf.convert_to_tensor(Omega0_m,dtype=tf.float32)
+    cosmology['sigma8']=tf.convert_to_tensor(sigma8,dtype=tf.float32)
+    init_stages = np.linspace(a0, af, n_steps, endpoint=True)
+    initial_conditions = flowpm.linear_field(nc,    
+                                            Boxsize, 
+                                             lambda k: tf.cast(linear_matter_power(cosmo, k), tf.complex64),         
+                                             batch_size=1)
+    # Sample particles
+    state = flowpm.lpt_init(initial_conditions, 0.1)   
+    # Evolve particles down to z=0
+    final_state = flowpm.nbody(state, init_stages, nc)         
+    # Retrieve final density field
+    state, lps_a, lps=lightcone(final_state, a[::-1], 
+                                  nc, 
+                                    field*60/plane_size, plane_size,
+              cosmology)
+    k_map=Born(lps_a,lps,ds,nc,Boxsize,plane_size,field,cosmology)
     k_map=tf.cast(k_map,dtype=tf.complex64)
-    data_ft = tf.signal.fftshift(tf.signal.fft2d(k_map)) / k_map.shape[0]
-    nyquist = tf.cast(k_map.shape[0]/2,dtype=tf.int32)
-    data=tf.math.real(data_ft*tf.math.conj(data_ft))
-    center = data.shape[0]/2
-    y, x = np.indices((data.shape))
-    r = tf.math.sqrt((x - center)**2 + (y - center)**2)
-    r=tf.cast(r,dtype=tf.int32)
-    tbin=tf.math.bincount(tf.reshape(r,[-1]), tf.reshape(data,[-1]))
-    nr = tf.math.bincount(tf.reshape(r,[-1]))
-    radialprofile=tf.cast(tbin,dtype=tf.float64)/tf.cast(nr,dtype=tf.float64)
-    power_spectrum = radialprofile[:nyquist]
-    power_spectrum = power_spectrum*pixel_size_tf**2
-    k = tf.range(power_spectrum.shape[0],dtype=tf.float64)
-    ell = 2. * tf.constant(np.pi,dtype=tf.float64) * k / tf.constant(pixel_size_tf,dtype=tf.float64) / tf.cast(k_map.shape[0],dtype=tf.float64)
-    return ell, power_spectrum
+    ell, power_spectrum=measure_power_spectrum_tf(k_map,field,plane_size)
+    return ell, power_spectrum, k_map
+
