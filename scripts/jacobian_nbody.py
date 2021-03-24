86992be48ed10c01f8f04c701e183053380682b8
LANZIERI Denise <deniselanzieri@gmail.com>
2021-03-24 23:42:45 +0100

    k-bin changed

--- scripts/jacobian_nbody.py
+++ scripts/jacobian_nbody.py
@@ -37,37 +37,35 @@ def compute_Nbody(Omega_c, sigma8):
   """
   # Instantiates a cosmology with desired parameters
   cosmology = flowpm.cosmology.Planck15(Omega_c=Omega_c, sigma8=sigma8)
-
+  stages = np.linspace(0.1, 1., FLAGS.nsteps, endpoint=True)
   # Compute linear matter power spectrum
   k = tf.constant(np.logspace(-4, 1, 256), dtype=tf.float32)
   pk = tfpower.linear_matter_power(cosmology, k)
   pk_fun = lambda x: tf.cast(tf.reshape(interpolate.interp_tf(tf.reshape(tf.cast(x, tf.float32), [-1]), k, pk), x.shape), tf.complex64)
 
   # And initial conditions
-  initial_conditions = flowpm.linear_field([FLAGS.nc, FLAGS.nc, 10 * FLAGS.nc],
+  initial_conditions = flowpm.linear_field([FLAGS.nc, FLAGS.nc, FLAGS.nc],
                                            [FLAGS.box_size, FLAGS.box_size,
-                                           10 * FLAGS.box_size],
+                                           FLAGS.box_size],
                                            pk_fun,
                                            batch_size=1)
 
-  r = tf.linspace(0., 2000, FLAGS.nsteps)
-  a = flowpm.tfbackground.a_of_chi(cosmology, r)
+  # r = tf.linspace(0., 200, FLAGS.nsteps)
+  # a = flowpm.tfbackground.a_of_chi(cosmology, r)
 
   # Sample particles, using LPT up to the border of the lightcone
   # WARNING: this is probably very approximate
-  state = flowpm.lpt_init(cosmology, initial_conditions, a[-1])
+  state = flowpm.lpt_init(cosmology, initial_conditions, 0.1)
 
   
    # Evolve particles from initial state down to a=af
-  final_state = flowpm.nbody(cosmology, state, a, [FLAGS.nc, FLAGS.nc, 10 * FLAGS.nc])         
+  final_state = flowpm.nbody(cosmology, state, stages, [FLAGS.nc, FLAGS.nc,  FLAGS.nc])         
 
   # Retrieve final density field i.e interpolate the particles to the mesh
   final_field = flowpm.cic_paint(tf.zeros_like(initial_conditions), final_state[0])
-  final_field=tf.reshape(final_field, [FLAGS.nc, FLAGS.nc, 10 * FLAGS.nc])
+  final_field=tf.reshape(final_field, [FLAGS.nc, FLAGS.nc, FLAGS.nc])
   return final_field
-#%%
 
-#bvv=tf.reshape(final_field, [nc, nc,10* nc])
 
 #%%
 @tf.function
@@ -78,7 +76,8 @@ def compute_jacobian(Omega_c, sigma8):
   with tf.GradientTape() as tape:
     tape.watch(params)
     final_field = compute_Nbody(params[0], params[1])
-    k, power_spectrum = pkl(final_field,kmin=0.01,dk=0.01,shape=final_field.shape,boxsize=np.array([FLAGS.box_size, FLAGS.box_size, 10 * FLAGS.box_size]))
+    k, power_spectrum = pkl(final_field,shape=final_field.shape,boxsize=np.array([FLAGS.box_size, FLAGS.box_size,
+                                           FLAGS.box_size]),kmin=0.1,dk=0.03)
 
   return final_field, tape.jacobian(power_spectrum, params,experimental_use_pfor=False), k, power_spectrum
 
@@ -86,15 +85,17 @@ def compute_jacobian(Omega_c, sigma8):
 
 def main(_):
   # Query the jacobian
-   final_field, jacobian, k, power_spectrum = compute_jacobian(tf.convert_to_tensor(FLAGS.Omega_c,
-                                                             dtype=tf.float32),
-                                        tf.convert_to_tensor(FLAGS.sigma8,
-                                                             dtype=tf.float32))
+ 
+    final_field, jacobian, k, power_spectrum = compute_jacobian(tf.convert_to_tensor(FLAGS.Omega_c,
+                                                              dtype=tf.float32),
+                                          tf.convert_to_tensor(FLAGS.sigma8,
+                                                              dtype=tf.float32))
  
   # Saving results in requested filename
-   pickle.dump({'final_field': final_field.numpy(), 'k': k.numpy(),
-                'power_spectrum': power_spectrum.numpy(), 'jac': jacobian.numpy()},
-               open(FLAGS.filename, "wb"))
+    pickle.dump({'final_field': final_field.numpy(), 'k': k.numpy(),
+                  'power_spectrum': power_spectrum.numpy(), 'jac': jacobian.numpy()},
+                open(FLAGS.filename, "wb"))
+
   
 
 
